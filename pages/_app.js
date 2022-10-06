662bd46ef7989bcc08755b044c4c0b9705f8443d
hiihbcb <diabloeatmice@gmail.com>
2022-10-07 00:48:27 +0100

    inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../prismicio'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider
+      internalLinkComponent={({ href, ...props }) => (
+        <Link href={href}>
+          <a {...props} />
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
 
 export default MyApp
